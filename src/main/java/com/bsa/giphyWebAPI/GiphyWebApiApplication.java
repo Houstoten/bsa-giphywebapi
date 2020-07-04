e034fac1981f43f555b8dfef45150aa7d1dce66f
Houston <ivan.husarov@gmail.com>
2020-07-04 22:31:15 +0300

    boot init

---
+++ src/main/java/com/bsa/giphyWebAPI/GiphyWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.bsa.giphyWebAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiphyWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiphyWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsa/giphyWebAPI/GiphyWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsa.giphyWebAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiphyWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
